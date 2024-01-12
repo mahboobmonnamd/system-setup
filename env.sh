ea8dffb4c2e3d1fc881e85deda88ea882dc7c600
mahboobmonnamd <129930859+mahboobmonnamd@users.noreply.github.com>
2024-01-12 16:20:55 +0530

    Create env.sh

---
+++ env.sh
@@ -0,0 +1,22 @@
+#!/bin/zsh
+
+# Add commonly used folders to $PATH
+export NVM_DIR="$HOME/.nvm"
+  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+export GOPATH=$HOME/Developer/.go
+export MAKEPATH="/opt/homebrew/Cellar/make/4.4.1/libexec/gnubin"
+# export PATH="$PATH:$HOME/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$GOROOT/bin:$GOPATH/bin:$MAKEPATH"
+export PATH=$MAKEPATH:$GOPATH/bin:/opt/homebrew/opt/openjdk/bin:${KREW_ROOT:-$HOME/.krew}/bin:$PATH
+
+# Specify default editor. Possible values: vim, nano, ed etc.
+export EDITOR=code
+
+# Create a folder and move into it in one command
+function mkcd() { mkdir -p "$@" && cd "$_"; }
+
+# Example aliases
+alias zshconfig="code ~/.zshrc"
+alias ohmyzsh="code ~/.oh-my-zsh"
+alias envconfig="code ~/shell/env.sh"
+alias python="python3"
