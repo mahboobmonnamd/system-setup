ffd9e9f9bb53f6a7e3bba0788f8cad5d9c8ba8a3
mahboobmonnamd <129930859+mahboobmonnamd@users.noreply.github.com>
2024-01-13 07:26:59 +0530

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,13 +1,14 @@
 #!/bin/zsh
 
 # Add commonly used folders to $PATH
+export ANDROID_HOME=~/Library/Android/sdk
 export NVM_DIR="$HOME/.nvm"
   [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
   [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 export GOPATH=$HOME/Developer/.go
 export MAKEPATH="/opt/homebrew/Cellar/make/4.4.1/libexec/gnubin"
 # export PATH="$PATH:$HOME/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$GOROOT/bin:$GOPATH/bin:$MAKEPATH"
-export PATH=$MAKEPATH:$GOPATH/bin:/opt/homebrew/opt/openjdk/bin:${KREW_ROOT:-$HOME/.krew}/bin:$PATH
+export PATH=$MAKEPATH:$GOPATH/bin:/opt/homebrew/opt/openjdk/bin:${KREW_ROOT:-$HOME/.krew}/bin:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$PATH
 
 # Specify default editor. Possible values: vim, nano, ed etc.
 export EDITOR=code
