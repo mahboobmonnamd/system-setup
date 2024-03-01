2bf2fd06fe8c8c344d148b2f794a3576a949c664
mahboobmonnamd <129930859+mahboobmonnamd@users.noreply.github.com>
2024-03-01 22:24:54 +0530

    Update install.sh

--- apps/install.sh
+++ apps/install.sh
@@ -11,9 +11,12 @@ brew doctor —verbose
 sudo gem install colorls
 colorls --light
 
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"
 nvm install --lts
 npm i -g typescript ts-node
 
 info '##### Installting oh my zsh#######'
+
+chmod +x ./oh-my-zsh.sh
 
 ./oh-my-zsh.sh
