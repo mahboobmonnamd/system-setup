89a101b306d9d8454afc60b07c86277da8f85d53
mahboobmonnamd <129930859+mahboobmonnamd@users.noreply.github.com>
2024-03-01 22:32:52 +0530

    Update bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -22,6 +22,7 @@ install_homebrew() {
 		info "Installing homebrew..."
 		sudo --validate # reset `sudo` timeout to use Homebrew install in noninteractive mode
 		NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+		eval "$(/opt/homebrew/bin/brew shellenv)"
 	fi
 }
 
@@ -29,10 +30,16 @@ info "####### dotfiles #######"
 read -p "Press enter to start:"
 info "Bootstraping..."
 
-./osx.sh
+    # Ask for the administrator password upfront
+    sudo -v
+
+    # Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+    while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 install_xcode
 install_homebrew
 
-cd ../apps && ./brew.sh
+ln -s ../apps/Brewfile ~
+../apps/install.sh
+
 ./osx.sh
