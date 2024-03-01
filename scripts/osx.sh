06cf6246fd000e3df4d0068eacca5d7398b91c22
mahboobmonnamd <129930859+mahboobmonnamd@users.noreply.github.com>
2024-03-01 22:25:29 +0530

    Update osx.sh

--- scripts/osx.sh
+++ scripts/osx.sh
@@ -8,13 +8,8 @@ setup_osx_defaults() {
     # settings we’re about to change
     osascript -e 'tell application "System Preferences" to quit'
 
-    # Ask for the administrator password upfront
-    sudo -v
 
-    # Keep-alive: update existing `sudo` time stamp until `.macos` has finished
-    while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
-    sudo softwareupdate -ia —verbose;
+    # sudo softwareupdate -ia —verbose
     
     ###############################################################################
     # General UI/UX                                                               #
